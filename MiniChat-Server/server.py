dd3ed32ed71db880302d6dc41e66f56261ae8962
MisakMikot <wly421@163.com>
2023-06-25 23:18:44 +0800

    Initial commit

---
+++ MiniChat-Server/server.py
@@ -0,0 +1,189 @@
+import socket
+import pymysql
+import logging
+import os
+import time
+from threading import Thread
+
+# 数据库信息
+DATABASE = {
+    "address": "127.0.0.1",
+    "port": 3306,
+    "user": "minichat",
+    "password": "MiniDB",
+    "dbname": "minichat",
+}
+SERVER = {"bind": "0.0.0.0", "port": 9898}
+
+conns = []
+
+try:
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    if not os.path.isdir("logs"):
+        os.mkdir("logs")
+    log = logging.getLogger(__name__)
+    log.setLevel(logging.DEBUG)
+    # 创建 handler 输出到文件
+    file_handler = logging.FileHandler(
+        "logs/" + str(round(time.time())) + ".log", mode="w", encoding="utf-8"
+    )  # 一定要加上 mode='w'
+    file_handler.setLevel(logging.DEBUG)
+    qt_handler = logging.Handler()
+    # handler 输出到控制台
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    # 创建 logging format
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(funcName)s:%(lineno)d] [%(name)s/%(levelname)s] : %(message)s"
+    )
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+    # add the handlers to the logger
+    log.addHandler(file_handler)
+    log.addHandler(console_handler)
+    log.info("日志系统上线!")
+except Exception as e:
+    print(e)
+    print("日志系统启动失败")
+
+
+def InitDb():
+    # 初始化数据库
+    try:
+        # 尝试连接
+        log.info("尝试连接到数据库（{}:{}）".format(DATABASE["address"], DATABASE["port"]))
+        db = pymysql.connect(
+            host=DATABASE["address"],
+            user=DATABASE["user"],
+            passwd=DATABASE["password"],
+            port=DATABASE["port"],
+            db=DATABASE["dbname"],
+        )
+        log.info("成功连接到数据库！")
+    except:
+        log.critical("----------CRITICAL----------")
+        log.critical("连接到数据库失败！！！", exc_info=True)
+        log.critical("程序正在退出......")
+        log.critical("----------CRITICAL----------")
+        exit()
+    log.debug("创建游标对象")
+    cursor = db.cursor()
+    try:
+        # 初始化用户表
+        log.debug("尝试建表：users")
+        sql = """
+        CREATE TABLE `users`  (
+        `uuid` int NOT NULL,
+        `nickname` varchar(255) NOT NULL,
+        `avatar_id` int NOT NULL,
+        `sex` int NOT NULL,
+        `password` varchar(255) NOT NULL,
+        `loginname` varchar(255) NOT NULL
+        );
+        """
+        cursor.execute(sql)
+        log.debug("新建的表：users")
+    except Exception as e:
+        if "1050" in str(e):
+            log.debug("表：users 已存在")
+        else:
+            log.error("建表失败！", exc_info=True)
+
+    try:
+        # 初始化好友列表
+        log.debug("尝试建表：friends")
+        sql = """
+        CREATE TABLE `friends`  (
+        `uuid` int NOT NULL,
+        `friend_id` int NOT NULL
+        );
+        """
+        cursor.execute(sql)
+        log.debug("新建的表：friends")
+    except Exception as e:
+        if "1050" in str(e):
+            log.debug("表：friends 已存在")
+        else:
+            log.error("建表失败！", exc_info=True)
+
+    try:
+        # 初始化聊天列表
+        log.debug("尝试建表： chats")
+        sql = """
+        CREATE TABLE `chats`  (
+        `uuid` int NOT NULL,
+        `friend_id` int NOT NULL,
+        `chat_id` int NOT NULL
+        );
+        """
+        cursor.execute(sql)
+        log.debug("新建的表：chats")
+    except Exception as e:
+        if "1050" in str(e):
+            log.debug("表：chats 已存在")
+        else:
+            log.error("建表失败！", exc_info=True)
+
+    try:
+        # 初始化私聊消息列表
+        log.debug("尝试建表： chatmessages")
+        sql = """
+        CREATE TABLE `chatmessages`  (
+        `uuid` int NOT NULL,
+        `chat_id` int NOT NULL,
+        `message` longtext,
+        `file_id` int,
+        `photo_id` int
+        );
+        """
+        cursor.execute(sql)
+        log.debug("新建的表：chatmessages")
+    except Exception as e:
+        if "1050" in str(e):
+            log.debug("表：chatmessages 已存在")
+        else:
+            log.error("建表失败！", exc_info=True)
+
+
+def CmdHandle():
+    log.info("命令系统上线")
+    try:
+        while True:
+            cmd = input("> ")
+            if cmd == '':
+                continue
+    except Exception as e:
+        log.error("命令系统出现错误，请重启服务器！", exc_info=True)
+
+
+def InitSvr():
+    # 创建socket对象
+    sock = socket.socket()
+    try:
+        # 尝试绑定服务器
+        sock.bind((SERVER["bind"], SERVER["port"]))
+        sock.listen(1000)
+        log.info("服务器运行在地址（{}:{}）".format(SERVER["bind"], SERVER["port"]))
+    except Exception as e:
+        exc = str(e)
+        log.critical("----------CRITICAL----------")
+        log.critical(
+            "无法绑定服务器到地址（{}:{}）！！".format(SERVER["bind"], SERVER["port"]), exc_info=True
+        )
+        log.critical("程序正在退出！")
+        log.critical("----------CRITICAL----------")
+        exit()
+    tcmd = Thread(target=CmdHandle)
+    tcmd.start()
+    while True:
+        s, addr = sock.accept()
+        log.info("传入连接：{}".format(str(addr)))
+
+
+def MsgHandle(s, addr):
+    pass
+
+
+if __name__ == "__main__":
+    InitDb()
+    InitSvr()
---
+++ launcher.py
@@ -0,0 +1,209 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import Qt, pyqtSignal, QObject
+from Ui_launcher import Ui_launcherui
+import sys
+import os
+import socket
+import base64
+from qfluentwidgets import InfoBar, InfoBarPosition
+from threading import Thread
+from qframelesswindow import FramelessMainWindow
+
+s = socket.socket()
+connected = False
+
+
+class MyLauncher(QMainWindow):
+    # 重写QMainWindow类
+    pass
+
+
+class slots(QObject):
+    # 定义槽函数
+    def error(self, title: str, content: str) -> None:
+        # 显示错误信息
+        InfoBar.error(
+            title=title,
+            content=content,
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM_RIGHT,
+            duration=-1,  # won't disappear automatically
+            parent=lclass,
+        )
+
+    def loadon(self):
+        # 显示加载条
+        launcher.IndeterminateProgressBar.show()
+
+    def loadoff(self):
+        # 隐藏加载条
+        launcher.IndeterminateProgressBar.hide()
+
+    def info(self, title, content):
+        # 显示错误信息
+        InfoBar.info(
+            title=title,
+            content=content,
+            orient=Qt.Horizontal,
+            isClosable=True,
+            position=InfoBarPosition.BOTTOM_RIGHT,
+            duration=-1,  # won't disappear automatically
+            parent=lclass,
+        )
+
+    def unlock(self):
+        launcher.ledit_account.setDisabled(False)
+        launcher.ledit_password.setDisabled(False)
+        launcher.ledit_passwordre.setDisabled(False)
+        launcher.btn_login.setDisabled(False)
+        launcher.btn_register.setDisabled(False)
+        launcher.swich_savelogin.setDisabled(False)
+        launcher.ledit_addr.setDisabled(True)
+        launcher.sbox_port.setDisabled(True)
+
+    def lock(self):
+        launcher.ledit_account.setDisabled(True)
+        launcher.ledit_password.setDisabled(True)
+        launcher.ledit_passwordre.setDisabled(True)
+        launcher.btn_login.setDisabled(True)
+        launcher.btn_register.setDisabled(True)
+        launcher.swich_savelogin.setDisabled(True)
+        launcher.ledit_addr.setDisabled(False)
+        launcher.sbox_port.setDisabled(False)
+
+
+class ui(QObject):
+    # 定义信号
+    signal_error = pyqtSignal(str, str)
+    signal_loadon = pyqtSignal()
+    signal_loadoff = pyqtSignal()
+    signal_info = pyqtSignal(str, str)
+    signal_unlock = pyqtSignal()
+    signal_lock = pyqtSignal()
+    
+
+    def __init__(self):
+        super(ui, self).__init__()
+
+    def error(self, title, content):
+        # 显示错误信息
+        self.signal_error.emit(title, content)
+
+    def loadon(self):
+        # 显示加载条
+        self.signal_loadon.emit()
+
+    def loadoff(self):
+        # 隐藏加载条
+        self.signal_loadoff.emit()
+
+    def info(self, title, content):
+        self.signal_info.emit(title,content)
+
+    def lock(self):
+        self.signal_lock.emit()
+
+    def unlock(self):
+        self.signal_unlock.emit()
+
+
+def msgHaldle(s):
+    while True:
+        try:
+            msg = s.recv(1024).decode('utf-8')
+        except Exception as e:
+            if '10038' in str(e):
+                ui.lock()
+                ui.info(title='信息', content='连接断开')
+                return
+            else:
+                ui.error(title='错误', content=str(e))
+            
+        print(msg)
+
+
+def connect():
+    global s
+    global connected
+    if connected:
+        #断开连接
+        s.close()
+        launcher.btn_conn.setText('连接')
+        connected = False
+        s = socket.socket()
+        ui.lock()
+        return
+    #获取地址
+    addr = launcher.ledit_addr.text()
+    port = launcher.sbox_port.text()
+    if addr == "":
+        #检查地址
+        ui.error("错误", "服务器地址应该被填写")
+        return
+    ui.loadon()
+    try:
+        #获取IP
+        server_ip = socket.gethostbyname(addr)
+    except Exception as e:
+        ui.error(title="定位服务器失败", content=str(e))
+        ui.loadoff()
+        return
+    try:
+        #连接服务器
+        s.connect((server_ip, int(port)))
+    except Exception as e:
+        ui.error(title="连接服务器失败", content=str(e))
+        ui.loadoff()
+        return
+    ui.loadoff()
+    ui.info(title='信息',content='连接到服务器')
+    launcher.btn_conn.setText('断开')
+    connected = True
+    ui.unlock()
+    t = Thread(target=msgHaldle, kwargs={'s':s})
+    t.start()
+
+
+def slotConn():
+    # 信号槽连接
+    launcher.btn_conn.clicked.connect(lambda: Thread(target=connect).start())
+    # 自定义信号连接
+    ui.signal_error.connect(slots.error)
+    ui.signal_loadon.connect(slots.loadon)
+    ui.signal_loadoff.connect(slots.loadoff)
+    ui.signal_info.connect(slots.info)
+    ui.signal_lock.connect(slots.lock)
+    ui.signal_unlock.connect(slots.unlock)
+
+
+def main():
+    # 程序主函数
+    if not os.path.isdir("data"):
+        # 检查数据目录
+        os.mkdir("data")
+    # if not os.path.isfile('data/login.data'):
+    # 检查登录信息
+    # with open('data/login.data'):
+    # pass
+
+
+if __name__ == "__main__":
+    # 程序入口
+    # enable dpi scale
+    QApplication.setHighDpiScaleFactorRoundingPolicy(
+        Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
+    )
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
+    app = QApplication(sys.argv)
+    lclass = MyLauncher()
+    launcher = Ui_launcherui()
+    launcher.setupUi(lclass)
+    launcher.IndeterminateProgressBar.hide()
+    ui = ui()
+    slots = slots()
+    slotConn()
+    lclass.show()
+    sys.exit(app.exec_())
